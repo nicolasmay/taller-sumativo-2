5febcbd37bd5a2030b31efeaa034b6581899dba5
Nicolás González Mayorga <64341232+nicolasmay@users.noreply.github.com>
2023-07-19 23:07:39 -0400

    Add files via upload

---
+++ ngm.socket/Cliente/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using ClienteSocketUtils;
+
+namespace ClienteImz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conexion al servidor
+            int puerto;
+            string servidor;
+            
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Ingrese la ip del servidor: ");
+                Console.ResetColor();
+                servidor = Console.ReadLine().Trim();
+                
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Ingrese el puerto: ");
+                Console.ResetColor();
+                puerto = Convert.ToInt32(Console.ReadLine().Trim());
+                
+
+                // Creamos el socket para la conexion al servidor
+                ClienteSocket clienteSocket = new ClienteSocket(servidor, puerto);
+
+                // Validamos la conexion
+                if (clienteSocket.Conectar())
+                {
+                    //OK, puede conectar
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("                     Enhorabuena!! conexion exitosa...");
+                    Console.ResetColor();
+
+                    //aqui esta el protocolo de comunicacion
+                    bool seguir = true;
+                    string respuesta;
+                    string mensaje_server;
+                    do
+                    {
+                        respuesta = clienteSocket.Leer();
+                        Console.WriteLine("Servidor: " + respuesta);
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write("                             Envia una respuesta: ");
+                        Console.ResetColor();
+                        mensaje_server = Console.ReadLine().Trim();
+                        clienteSocket.Escribir(mensaje_server);
+                        if (respuesta == "chao" || mensaje_server == "chao")
+                        {
+                            seguir = false;
+                            clienteSocket.Desconectar();
+
+                            Console.ReadKey();
+
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine("                        Te has desconectado del servidor...");
+                            Console.ResetColor();
+                        }
+                    } while (seguir);
+                }
+                else
+                {
+                    Console.WriteLine("              Error de conexion, el puerto {0} esta en uso", puerto);
+                }
+            }
+        }
+    }
+}
---
+++ ngm.socket/socket/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using ServidorSocketUtils;
+
+namespace imz.sockt
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+
+            Console.WriteLine("Inicializando el servidor en el puerto.... {0}", puerto);
+            ServerSocket servidor = new ServerSocket(puerto);
+
+            if (servidor.Iniciar())
+            {
+                //OK, puede conectar
+                
+                Console.WriteLine("       Servidor iniciado.....");
+                
+                
+                while (true)
+                {
+                    //Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    //Console.WriteLine("                           Esperando Cliente...");
+                    //Console.ResetColor();
+                    Socket socketCliente = servidor.ObtenerCliente();
+
+                    //construir el mecanismo para escribir y leeer cliente
+                    ClienteCom cliente = new ClienteCom(socketCliente);
+                    //aqui esta el protocolo de comunicacion
+                    bool salir = true;
+                    string respuesta;
+                    string mensaje_server;
+                    do
+                    //<! Establecer la conversacion
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write("                               Escribe algo: ");
+                        Console.ResetColor();
+                        mensaje_server = Console.ReadLine().Trim();
+                        cliente.Escribir(mensaje_server);
+                        respuesta = cliente.Leer();
+                        Console.WriteLine("Cliente: " + respuesta);
+                        
+                        
+                        if (respuesta == "chao" || mensaje_server == "chao") 
+                        {
+                            salir = false;
+                            cliente.Desconectar();
+
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine("                         El cliente se desconecto del servidor... ");
+                            Console.ResetColor();
+                        }
+                    }while (salir);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Error, intentalo nuevamente !!");
+            }
+        }
+    }
+}
